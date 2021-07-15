7ed1c1ddd04b15d233d3d3a79410a5496b9b3d7d
RizkyFajriawan23 <87220589+RizkyFajriawan23@users.noreply.github.com>
2021-07-15 13:05:42 +0700

    coba layout

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Rizky Fajriawan",
+        "email" => "fajriawan.183040037@mail.unpas.id",
+        "image" => "img/1.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
